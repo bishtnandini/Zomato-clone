71b85a594952565a2c68529d7af4376ff327280d
technandini <83299824+technandini@users.noreply.github.com>
2023-07-24 19:21:27 +0530

    Add files via upload

---
+++ dining out/java.js
@@ -0,0 +1,10 @@
+function scrollls() {
+    var right = document.querySelector(".scrolls-images");
+    right.scrollBy(-350, 0)
+
+}
+
+function scrollsr() {
+    var left = document.querySelector(".scrolls-images");
+    left.scrollBy(350, 0)
+}
---
+++ main/java.js
@@ -0,0 +1,21 @@
+function scrolll() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(-350, 0)
+
+}
+
+function scrollr() {
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(350, 0)
+}
+
+function scrol() {
+    var right = document.querySelector(".scrol-images");
+    right.scrollBy(-350, 0)
+
+}
+
+function scrolr() {
+    var left = document.querySelector(".scrol-images");
+    left.scrollBy(350, 0)
+}
